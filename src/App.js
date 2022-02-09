e1714b70a782767ce656954b95ff583b5c2fc0b2
elitekaycy <dicksonanyaele1234@gmail.com>
2022-02-09 12:28:39 +0000

    title change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 import ImageBox from './components/ImageProcess/ImageBox';
 import NavigationBar from './components/navbarComponent/NavigationBar';
+import React from 'react' 
 
 function App() {
+
+  React.useEffect(() => {
+    //eslint-disable-next-line
+    let mounted = true
+
+    document.title = "imagenation"
+
+    return () => mounted = false
+  })
+
   return (
     <div className="relative w-screen h-screen bg-slate-100 flex flex-col overflow-hidden space-y-8">
       <NavigationBar />
